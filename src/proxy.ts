be811be0456428171988c252949d32d956e05379
aalnoman2042 <abdullahalnoman2042@gmail.com>
2025-11-29 01:17:14 +0600

    proxy file setup

---
+++ src/proxy.ts
@@ -0,0 +1,13 @@
+
+import type { NextRequest }         
+from "next/server";
+import { NextResponse } from "next/server";
+
+
+export function proxy(request: NextRequest) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+}
+
+export const config = {
+    matcher: '/',
+};
